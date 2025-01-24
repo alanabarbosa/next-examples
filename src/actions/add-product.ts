54f8e3c68b065e7ccf67263326ef38759b5fc554
Alana Barbosa <itsalanayall@hotmail.com>
2025-01-24 12:59:56 -0300

    Add example form state

--- src/actions/add-product.ts
+++ src/actions/add-product.ts
@@ -5,15 +5,48 @@ import { Product } from '@/components/server-fetch';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-export async function addProduct(product: Product) {
-  const response = await fetch('https://api.origamid.online/produtos', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(product),
-  });
-  await response.json();
+const validateName = (nome: string) => {
+  return typeof nome === 'string' && nome.length > 1;
+}
+
+const validatePrice = (preco: unknown) => {
+  return typeof preco === 'number' && preco > 1;
+}
+
+export async function addProduct(
+  state: { errors: string[] },
+  formData: FormData,) {
+
+  const product: Product = {
+    nome: formData.get('nome') as string,
+    preco: Number(formData.get('preco')) as number,
+    descricao: formData.get('descricao') as string,
+    estoque: Number(formData.get('estoque')) as number,
+    importado: formData.get('importado') ? 1 : 0
+  };
+
+  let errors = [];
+  if (!validateName(product.nome)) errors.push('invalid name');
+  if (!validatePrice(product.preco)) errors.push('invalid price');
+  if (errors.length > 0) return {errors};
+
+  try {
+    const response = await fetch('https://api.origamid.online/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(product),
+    });
+    if (!response.ok) throw new Error('Erro ao adicionar o produto.');
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: [error.message],
+      };
+    }
+  }
   revalidatePath('/products');
   redirect('/products');
+  //return {errors: []};
 }
--- src/components/add-product.tsx
+++ src/components/add-product.tsx
@@ -1,11 +1,20 @@
 'use client';
 
 import { addProduct } from '@/actions/add-product';
+import { useFormState, useFormStatus } from 'react-dom';
 //import { Product } from '@/app/products/page';
-import { Product } from '@/components/server-fetch';
+//import { Product } from '@/components/server-fetch';
+
+const Button = () => {
+  const status = useFormStatus();
+  return <button type='submit' 
+    disabled={status.pending}>
+      {status.pending ? 'Adding...' : 'Add'}
+    </button>
+}
 
 export default function AddProduct() {
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  /*async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const data: Product = {
       nome: event.currentTarget.nome.value,
@@ -15,23 +24,40 @@ export default function AddProduct() {
       importado: event.currentTarget.importado.checked ? 1 : 0,
     };
     await addProduct(data);
-  }
+  }*/
+  
+  const [state, formAction] = useFormState(addProduct as any, {
+    errors: [],
+  });
+
+  console.log(state);
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={formAction}>
       <label htmlFor="nome">Name</label>
       <input type="text" id="nome" name="nome" />
+      
       <label htmlFor="preco">Price</label>
       <input type="number" id="preco" name="preco" />
+      
       <label htmlFor="descricao">Description</label>
       <input type="text" id="descricao" name="descricao" />
+      
       <label htmlFor="estoque">Stock</label>
       <input type="number" id="estoque" name="estoque" />
+      
       <label htmlFor="importado">
         <input type="checkbox" id="importado" name="importado" />
         Imported
       </label>
-      <button type="submit">Add</button>
+
+      {state.errors.map((error, i) => (
+        <p style={{ color: 'red' }} key={i}>
+          {error}
+        </p>
+      ))}
+
+      <Button />
     </form>
   );
 }
